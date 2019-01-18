9e36aa373ad7b188f2ced9e1b8633f67680a3364
Paulo Barros <paulofmb@hotmail.com>
2019-01-18 10:05:37 +0000

    First Commit

--- Projeto_Analise_Dados/Program.cs
+++ Projeto_Analise_Dados/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Projeto_Analise_Dados
 {
+
     class Program
     {
-        static void Main(string[] args)
+        
+
+        void Main(string[] args)
         {
+            
         }
+
+       
     }
+
 }
